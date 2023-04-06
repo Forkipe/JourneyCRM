3e0c02b9ddc6ebd06e33ce699d367af9e13424c8
Forkipe <mhorkiy@gmail.com>
2023-04-06 13:22:36 +0300

    Initial Commit

---
+++ JourneyCRM/JourneyCRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyCRMApp.swift
+//  JourneyCRM
+//
+//  Created by Марк Горкій on 06.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct JourneyCRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
